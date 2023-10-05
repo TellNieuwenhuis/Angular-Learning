e0c878d8b3285a8f6da807b611bada9103978ad3
Tell Nieuwenhuis <tell_nieuwenhuis@hotmail.com>
2023-10-04 18:53:28 -0700

    set active tab

--- clipz/src/app/shared/tabs-container/tabs-container.component.ts
+++ clipz/src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,20 +1,32 @@
-import { Component, AfterContentInit, ContentChildren,QueryList } from '@angular/core';
+import {
+  Component,
+  AfterContentInit,
+  ContentChildren,
+  QueryList,
+} from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
-  styleUrls: ['./tabs-container.component.scss']
+  styleUrls: ['./tabs-container.component.scss'],
 })
 export class TabsContainerComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs?: QueryList<TabComponent> =
+    new QueryList();
 
-  @ContentChildren(TabComponent) tabs?: QueryList<TabComponent> = new QueryList;
-
-  constructor() { }
+  constructor() {}
 
   ngAfterContentInit(): void {
-    this.tabs
-    console.log(this.tabs);
+    const activeTabs = this.tabs?.filter((tab) => tab.active);
+
+    if (!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first)
+    }
   }
 
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach(tab => tab.active = false);
+    tab.active = true;
+  }
 }
